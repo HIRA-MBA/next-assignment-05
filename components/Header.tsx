e7e9358ca9de3c92b45375e170a16ccaad1e7db0
HIRA-MBA <hirashamimqureshi@gmail.com>
2024-11-09 17:50:35 +0500

    assignment 05

---
+++ components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div style={{backgroundColor:'#A29875'}}className='w-[1920px] h-[134px]'>
+      <h1 className='w-[439px] h-[94px] text-[75px] font-normal text-white pt-[20px] pl-[36px]'>MANZZARI</h1>
+    </div>
+  )
+}
+
+export default Header
